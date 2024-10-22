4103e9597678e560b1d197ad2cb699debde5cda0
Lordarol <testdulite3@outlook.com>
2024-10-22 20:42:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
@@ -42,17 +42,17 @@ global.scan = "https://kgtech-v2-session.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "PUT YOUR SESSION HERE"
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia0lsS29tZE1EK3I1bWVtbmdjWlJFWU9oNUxYcEY0SFM1d0pDRWY3YzcyRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOWFCN2RPL1ZkTWRXc2lNSDRtWVI1OVZzUkZkZ2VOaU1GWU04SFNJWjAyRT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrRnJ2bW9SeXhNNzdwK1AzOUxqcUxoQVFlR1VRUTFhK2FhWE0xajZZc21vPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1MUx0VE9ySS9YQ0lyRWxidEk1VUI3SVdsZlRkL3B5THVpUXJ1UlJ1TmtJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImtKcEV5b1FXRHBja0F1aHVQekdTazdqRmR4QkgrUEp0bEI0ZEFNTE1rWGM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InM1dnV3RXMzMy80L1E5TDVScXNZb1ZyRTFhNHAyaTRKM2FSTlZnRmFaeDA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUF6cHg3TjdBREo0ZmQzM0s0SmdIMm1lZWt6WWc0TlNpRmtIRklHQ2pYVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSUhMWFRpMXlVdml0V080VGpMZ1hrU2FqMmdKYnE4T045OFljQ1g1Mm5rRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlPamx1R0t1YXRuRkc4MExkUHFYcU5saEFhcXI5QU5GVThOaEhENzhNdUpYWFVvU0pwUXUxNko1cHRKMGFrNW5CdmVVeTNCaUpiRUNjcCtVVlJnQ0NnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6ODEsImFkdlNlY3JldEtleSI6IjdOYUR3SlN2L2RYcTRCVlZlWE1Ma0RuSjA4SXhXTWJob2wvU0Fsb2lvZ0k9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMiwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMyLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IlQ1Q2xBRVdxU3otZlRFNEQ4Z0N5VnciLCJwaG9uZUlkIjoiZDJjMjczNjQtZWZjNS00MmRmLTgzNjctOGVkZmQ1NDFhZDYwIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlR1V0o1RVlJNGdLM0xFbHVBdlM2Y1BqemloMD0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJXY3FHdGg0OHRscFhNMS9EaHVqL29kK2ZQR1E9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiSEI0NVJUVzMiLCJtZSI6eyJpZCI6IjIzNzY1NjUyMDY3NDoyMUBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTFhjam84TUVKSCszN2dHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoidnZ5dHk5OER2Z1EwR3F6V1JXaU43UUU4RU9NeGtzWlVhTVZoN21LMHd5Zz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiZGZKSDNjeXl0R3ZhbUVXOURsNEoyUGJSUXRiNVdEMWQ5TkZ2dzZrbWJxeVV2Q0J5YVZ1NlB6MkEzK2dvOVRDWm1XdFc0LzhmNldCcmx5YndmM05YREE9PSIsImRldmljZVNpZ25hdHVyZSI6Ilp2RVVYT1BZVzdSV0t2L1F0NlB6aEdYekxZeVg5MGhoSmZIUk1DbElSblN2TTFGdWdnSmVpTlphbHBtanY3RzR0UnNSMXp0UGNXbXd3YWJNMFBmekF3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM3NjU2NTIwNjc0OjIxQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmI3OHJjdmZBNzRFTkJxczFrVm9qZTBCUEJEak1aTEdWR2pGWWU1aXRNTW8ifX1dLCJwbGF0Zm9ybSI6ImlwaG9uZSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyOTYyNTg4NiwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFOZnMifQ=="
 module.exports = {
   menu: process.env.MENU || "1",
-  HANDLERS: process.env.PREFIX || ".",
+  HANDLERS: process.env.PREFIX || "null",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`BRAND PRODUCT OF ☞⌜ KG TECH⌝☜`",
   author: process.env.PACK_AUTHER || "KG TECH",
   packname: process.env.PACK_NAME || "Kerm-MD-V2",
   botname: process.env.BOT_NAME || "ᵏᵍ┘𝙺𝙴𝚁𝙼_𝙼𝙳_𝚅𝟸𖤐",
-  ownername: process.env.OWNER_NAME || "☞⌜ KG TECH⌝☜",
+  ownername: process.env.OWNER_NAME || "☞⌜RAYAN⌝☜",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "tCxobE1FqJgfbDZgsYQNHBFB",
